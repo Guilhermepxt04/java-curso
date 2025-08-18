e02c65a3230bb0ac99c3a41fd43d3389c458a11f
Guilherme Peixoto <guilherme.pxt04@gmail.com>
2025-08-17 22:03:44 -0300

    feat: add Aula05EstruturaCondicional in package introducao

---
+++ src/edu/gui/study/aulas/introducao/Aula05EstruturaCondicional.java
@@ -0,0 +1,66 @@
+package edu.gui.study.aulas.introducao;
+
+import java.util.Scanner;
+
+public class Aula05EstruturaCondicional {
+
+    public static void main(String[] args) {
+
+        System.out.print("Que horas são?: ");
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+
+        if (x < 12) {
+            System.out.println("Bom dia");
+        }
+        else if (x < 18) {
+            System.out.println("Boa tarde");
+        }
+        else {
+            System.out.println("Boa noite");
+        }
+
+        // Switch-case
+
+        int y = sc.nextInt();
+        String dia;
+
+        switch (y) {
+            case 1:
+                dia = "Domingo";
+                break;
+            case 2:
+                dia = "Segunda";
+                break;
+            case 3:
+                dia = "Terça";
+                break;
+            case 4:
+                dia = "Quarta";
+                break;
+            case 5:
+                dia = "Quinta";
+                break;
+            case 6:
+                dia = "Sexta";
+                break;
+            case 7:
+                dia = "Sabado";
+                break;
+            default:
+                dia = "Valor invalido";
+                break;
+        }
+
+        System.out.println("Dia da semana: " + dia);
+        sc.close();
+
+        // Expressao condicional ternaria
+
+        String z = (5 < 10) ? "Maria" : "Alex";
+        System.out.println(z);
+
+    }
+}
+
