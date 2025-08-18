aa97e2c922e7223457341901388c4ba07b6afdc3
Guilherme Peixoto <guilherme.pxt04@gmail.com>
2025-08-17 21:03:55 -0300

    feat: add Aula04FuncoesMatematica

---
+++ src/edu/gui/study/aulas/introducao/Aula04FuncoesMatematica.java
@@ -0,0 +1,19 @@
+package edu.gui.study.aulas.introducao;
+
+public class Aula04FuncoesMatematica {
+
+    public static void main(String[] args) {
+
+        double x = 3.0;
+        double y = 4.0;
+        double z = -5.0;
+
+        System.out.println(Math.sqrt(x));
+        System.out.println(Math.pow(x, y));
+        System.out.println(Math.abs(z));
+        System.out.println(Math.abs(x));
+
+
+    }
+
+}
